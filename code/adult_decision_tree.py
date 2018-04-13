900304617d4495dceae027716e13c725d9f4ba33
Makan Arastuie <makan.arastuie@gmail.com>
2018-04-13 19:25:33 -0400

    Draw decision tree for adult

--- code/adult_decision_tree.py
+++ code/adult_decision_tree.py
@@ -5,29 +5,38 @@ from sklearn import tree
 import helpers.datasets.adult as adult
 from sklearn.metrics import accuracy_score
 
+# Loading the learning set
 adult_data = adult.load(adult.data_path, encode_labels=True)
 adult_data = adult.to_numpy_array(adult_data, remove_missing_values=True)
 
+# Separating to target and features
 adult_targets = adult_data[:, -1]
 adult_data = adult_data[:, 0:-1]
 
+# Training the classifier
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(adult_data, adult_targets)
 
+
+# Loading the test set
 adult_test = adult.load(adult.test_path, encode_labels=True)
 adult_test = adult.to_numpy_array(adult_test, remove_missing_values=True)
 
 adult_test_targets = adult_test[:, -1]
 adult_test = adult_test[:, 0:-1]
 
+# predicting
 adult_test_preds = clf.predict(adult_test)
 
-print(accuracy_score(adult_test_targets, adult_test_preds))
 
-dot_data = tree.export_graphviz(clf, out_file=None,
-                                feature_names=adult.feature_names,
-                                filled=True, rounded=True,
-                                special_characters=True,
-                                max_depth=5)
-graph = gviz.Source(dot_data)
-graph.render("Adult")
+print("Accuracy is: {0:3.2f}%".format(accuracy_score(adult_test_targets, adult_test_preds) * 100))
+
+# # Drawing the decision tree
+# dot_data = tree.export_graphviz(clf, out_file=None,
+#                                 feature_names=adult.feature_names,
+#                                 filled=True, rounded=True,
+#                                 special_characters=True,
+#                                 max_depth=5)
+# graph = gviz.Source(dot_data)
+# graph.render("Adult")
+
--- code/helpers/datasets/adult.py
+++ code/helpers/datasets/adult.py
@@ -4,58 +4,66 @@ import pandas as pd
 data_path = "../datasets/adult/adult.data.txt"
 test_path = "../datasets/adult/adult.test.txt"
 
+feature_names = ["Age", "Workclass", "fnlwgt", "Education", "Education-Num", "Martial Status", "Occupation",
+                 "Relationship", "Race", "Sex", "Capital Gain", "Capital Loss", "Hours per week", "Country"]
+
+feature_classes = {"Workclass": ["Private", "Self-emp-not-inc", "Self-emp-inc", "Federal-gov", "Local-gov",
+                                 "State-gov", "Without-pay", "Never-worked"],
+                   "Education": ["Bachelors", "Some-college", "11th", "HS-grad", "Prof-school", "Assoc-acdm",
+                                 "Assoc-voc",
+                                 "9th", "7th-8th", "12th", "Masters", "1st-4th", "10th", "Doctorate", "5th-6th",
+                                 "Preschool"],
+                   "Martial Status": ["Married-civ-spouse", "Divorced", "Never-married", "Separated", "Widowed",
+                                      "Married-spouse-absent", "Married-AF-spouse"],
+                   "Occupation": ["Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial",
+                                  "Prof-specialty", "Handlers-cleaners", "Machine-op-inspct", "Adm-clerical",
+                                  "Farming-fishing", "Transport-moving", "Priv-house-serv", "Protective-serv",
+                                  "Armed-Forces"],
+                   "Relationship": ["Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried"],
+                   "Race": ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"],
+                   "Sex": ["Female", "Male"],
+                   "Country": ["United-States", "Cambodia", "England", "Puerto-Rico", "Canada", "Germany",
+                               "Outlying-US(Guam-USVI-etc)", "India", "Japan", "Greece", "South", "China", "Cuba",
+                               "Iran",
+                               "Honduras", "Philippines", "Italy", "Poland", "Jamaica", "Vietnam", "Mexico",
+                               "Portugal",
+                               "Ireland", "France", "Dominican-Republic", "Laos", "Ecuador", "Taiwan", "Haiti",
+                               "Columbia",
+                               "Hungary", "Guatemala", "Nicaragua", "Scotland", "Thailand", "Yugoslavia",
+                               "El-Salvador",
+                               "Trinadad&Tobago", "Peru", "Hong", "Holand-Netherlands"]}
+
+target_classes = [">50K", "<=50K"]
+
 
 def load(path, encode_labels=False, verbose=False):
-    feature_names = ["Age", "Workclass", "fnlwgt", "Education", "Education-Num", "Martial Status", "Occupation",
-                     "Relationship", "Race", "Sex", "Capital Gain", "Capital Loss", "Hours per week", "Country",
-                     "Target"]
+    features_w_target = feature_names + ['Target']
 
     if not encode_labels:
-        adult_data = pd.read_csv(path, names=feature_names, sep=r'\s*,\s*', engine='python', na_values="?",
-                                 verbose=verbose)
+        return pd.read_csv(path, names=features_w_target, sep=r'\s*,\s*', engine='python',
+                           na_values="?", verbose=verbose)
 
-        return adult_data
+    encoders = {"Workclass": lambda x: feature_classes['Workclass'].index(x) if not pd.isnull(x) else x,
+                "Education": lambda x: feature_classes['Education'].index(x) if not pd.isnull(x) else x,
+                "Martial Status": lambda x: feature_classes['Martial Status'].index(x) if not pd.isnull(x) else x,
+                "Occupation": lambda x: feature_classes['Occupation'].index(x) if not pd.isnull(x) else x,
+                "Relationship": lambda x: feature_classes['Relationship'].index(x) if not pd.isnull(x) else x,
+                "Race": lambda x: feature_classes['Race'].index(x) if not pd.isnull(x) else x,
+                "Sex": lambda x: feature_classes['Sex'].index(x) if not pd.isnull(x) else x,
+                "Country": lambda x: feature_classes['Country'].index(x) if x is not None else x,
+                "Target": lambda x: target_classes.index(x.replace('.', '')) if not pd.isnull(x) else x}
 
-    dframe = {"Workclass": ["Private", "Self-emp-not-inc", "Self-emp-inc", "Federal-gov", "Local-gov", "State-gov",
-                            "Without-pay", "Never-worked"],
-              "Education": ["Bachelors", "Some-college", "11th", "HS-grad", "Prof-school", "Assoc-acdm", "Assoc-voc",
-                            "9th", "7th-8th", "12th", "Masters", "1st-4th", "10th", "Doctorate", "5th-6th",
-                            "Preschool"],
-              "Martial Status": ["Married-civ-spouse", "Divorced", "Never-married", "Separated", "Widowed",
-                                 "Married-spouse-absent", "Married-AF-spouse"],
-              "Occupation": ["Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial",
-                             "Prof-specialty", "Handlers-cleaners", "Machine-op-inspct", "Adm-clerical",
-                             "Farming-fishing", "Transport-moving", "Priv-house-serv", "Protective-serv",
-                             "Armed-Forces"],
-              "Relationship": ["Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried"],
-              "Race": ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"],
-              "Sex": ["Female", "Male"],
-              "Country": ["United-States", "Cambodia", "England", "Puerto-Rico", "Canada", "Germany",
-                          "Outlying-US(Guam-USVI-etc)", "India", "Japan", "Greece", "South", "China", "Cuba", "Iran",
-                          "Honduras", "Philippines", "Italy", "Poland", "Jamaica", "Vietnam", "Mexico", "Portugal",
-                          "Ireland", "France", "Dominican-Republic", "Laos", "Ecuador", "Taiwan", "Haiti", "Columbia",
-                          "Hungary", "Guatemala", "Nicaragua", "Scotland", "Thailand", "Yugoslavia", "El-Salvador",
-                          "Trinadad&Tobago", "Peru", "Hong", "Holand-Netherlands"],
-              "Target": [">50K", "<=50K"]}
-
-    encoders = {"Workclass": lambda x: dframe['Workclass'].index(x) if not pd.isnull(x) else x,
-                "Education": lambda x: dframe['Education'].index(x) if not pd.isnull(x) else x,
-                "Martial Status": lambda x: dframe['Martial Status'].index(x) if not pd.isnull(x) else x,
-                "Occupation": lambda x: dframe['Occupation'].index(x) if not pd.isnull(x) else x,
-                "Relationship": lambda x: dframe['Relationship'].index(x) if not pd.isnull(x) else x,
-                "Race": lambda x: dframe['Race'].index(x) if not pd.isnull(x) else x,
-                "Sex": lambda x: dframe['Sex'].index(x) if not pd.isnull(x) else x,
-                "Country": lambda x: dframe['Country'].index(x) if x is not None else x,
-                "Target": lambda x: dframe['Target'].index(x.replace('.', '')) if not pd.isnull(x) else x}
-
-    adult_data = pd.read_csv(path, names=feature_names, converters=encoders, sep='\s*,\s*', engine='python',
-                             na_values="?", verbose=verbose)
+    adult_data = pd.read_csv(path, names=features_w_target, converters=encoders, sep='\s*,\s*',
+                             engine='python', na_values="?", verbose=verbose)
 
     return adult_data
 
 
-def to_numpy_w_no_missing_value(panda_data_frame):
+def to_numpy_array(panda_data_frame, remove_missing_values=False):
     np_data = panda_data_frame.as_matrix()
+
+    if not remove_missing_values:
+        return np_data
 
     # Deleting rows with empty values
     np_data = np_data[~pd.isnull(np_data).any(axis=1)].astype(int)
