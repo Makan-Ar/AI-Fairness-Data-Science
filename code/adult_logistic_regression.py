229965337c5beefd0ccec9c4f8a68151f8ce963a
Makan Arastuie <makan.arastuie@gmail.com>
2018-04-29 15:43:41 -0400

    Add gaussian process classifier

--- code/adult_logistic_regression.py
+++ code/adult_logistic_regression.py
@@ -25,21 +25,22 @@ adult_test_features = adult_test[:, 0:-1]
 
 
 # Grid search on Logistic Regression
-param_grid = [{'penalty': ['l2'], 'C': [0.01, 0.1, 1, 5, 10, 100], 'tol': [1e-2, 1e-3, 1e-4, 1e-7, 1e-8],
-               'solver': ['lbfgs', 'liblinear', 'sag', 'saga']},
-              {'penalty': ['l1'], 'C': [0.01, 0.1, 1, 5, 10, 100], 'tol': [1e-2, 1e-3, 1e-4, 1e-7, 1e-8],
-               'solver': ['liblinear', 'saga']}]
-
-
-clf = make_pipeline(StandardScaler(), GridSearchCV(LogisticRegression(), param_grid=param_grid, n_jobs=25, cv=5, refit=True))
-clf.fit(adult_data, adult_targets)
-print("Best parameters set found on development set:\n")
-c = clf.named_steps.gridsearchcv.best_params_
-print(c)
-
-# Training the classifier -- This gets an accuracy of
-# clf = make_pipeline(StandardScaler(), LogisticRegression(C=6.6, penalty='l1', tol=0.01))
+# param_grid = [{'penalty': ['l2'], 'C': np.arange(0.1, 10, 0.1), 'tol': [1e-2, 1e-3, 1e-4, 1e-7, 1e-8],
+#                'solver': ['lbfgs', 'liblinear', 'sag', 'saga']},
+#               {'penalty': ['l1'], 'C': np.arange(0.1, 10, 0.1), 'tol': [1e-2, 1e-3, 1e-4, 1e-7, 1e-8],
+#                'solver': ['liblinear', 'saga']}]
+#
+#
+# clf = make_pipeline(StandardScaler(), GridSearchCV(LogisticRegression(), param_grid=param_grid,
+#                                                    n_jobs=25, cv=5, refit=True))
 # clf.fit(adult_data, adult_targets)
+# print("Best parameters set found on development set:\n")
+# c = clf.named_steps.gridsearchcv.best_params_
+# print(c)
+
+# Training the classifier -- This gets an accuracy of 83.80%
+clf = make_pipeline(StandardScaler(), LogisticRegression(C=6.6, penalty='l1', tol=0.01))
+clf.fit(adult_data, adult_targets)
 
 # predicting
 adult_test_preds = clf.predict(adult_test_features)
@@ -64,4 +65,4 @@ adult_test = adult.to_numpy_array(adult_test, remove_missing_values=True)
 # adult.evaluate_equality_of_opportunity(adult_test, clf, "Race")
 # adult.evaluate_equality_of_opportunity(adult_test, clf, "Sex")
 # adult.evaluate_equality_of_opportunity(adult_test, clf, "Country")
-# adult.evaluate_equality_of_opportunity(adult_test, clf, "Age")
+adult.evaluate_equality_of_opportunity(adult_test, clf, "Age")
