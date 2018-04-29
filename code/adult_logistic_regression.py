f3cef36430384700aa682cec1d6fc7fdc971f5a2
Makan Arastuie <makan.arastuie@gmail.com>
2018-04-28 20:59:20 -0400

    Add logistic regression grid search

--- code/adult_logistic_regression.py
+++ code/adult_logistic_regression.py
@@ -1,7 +1,11 @@
 import numpy as np
 import helpers.datasets.adult as adult
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
+
 
 # Loading the learning set
 adult_data = adult.load("learning", encode_features=True)
@@ -19,8 +23,23 @@ adult_test = adult.to_numpy_array(adult_test, remove_missing_values=True)
 adult_test_targets = adult_test[:, -1]
 adult_test_features = adult_test[:, 0:-1]
 
-clf = LogisticRegression(C=6.6, penalty='l1', tol=0.01)
-clf = clf.fit(adult_data, adult_targets)
+
+# Grid search on Logistic Regression
+param_grid = [{'penalty': ['l2'], 'C': [0.01, 0.1, 1, 5, 10, 100], 'tol': [1e-2, 1e-3, 1e-4, 1e-7, 1e-8],
+               'solver': ['lbfgs', 'liblinear', 'sag', 'saga']},
+              {'penalty': ['l1'], 'C': [0.01, 0.1, 1, 5, 10, 100], 'tol': [1e-2, 1e-3, 1e-4, 1e-7, 1e-8],
+               'solver': ['liblinear', 'saga']}]
+
+
+clf = make_pipeline(StandardScaler(), GridSearchCV(LogisticRegression(), param_grid=param_grid, n_jobs=25, cv=5, refit=True))
+clf.fit(adult_data, adult_targets)
+print("Best parameters set found on development set:\n")
+c = clf.named_steps.gridsearchcv.best_params_
+print(c)
+
+# Training the classifier -- This gets an accuracy of
+# clf = make_pipeline(StandardScaler(), LogisticRegression(C=6.6, penalty='l1', tol=0.01))
+# clf.fit(adult_data, adult_targets)
 
 # predicting
 adult_test_preds = clf.predict(adult_test_features)
@@ -37,12 +56,12 @@ print("Accuracy is: {0:3.2f}%".format(accuracy_score(adult_test_targets, adult_t
 # Evaluating demographic parity and equality of opportunity
 adult_test = adult.load("testing", encode_features=True)
 adult_test = adult.to_numpy_array(adult_test, remove_missing_values=True)
-adult.evaluate_demographic_parity(adult_test, clf, "Race")
-adult.evaluate_demographic_parity(adult_test, clf, "Sex")
-adult.evaluate_demographic_parity(adult_test, clf, "Country")
-adult.evaluate_demographic_parity(adult_test, clf, "Age")
-
-adult.evaluate_equality_of_opportunity(adult_test, clf, "Race")
-adult.evaluate_equality_of_opportunity(adult_test, clf, "Sex")
-adult.evaluate_equality_of_opportunity(adult_test, clf, "Country")
-adult.evaluate_equality_of_opportunity(adult_test, clf, "Age")
+# adult.evaluate_demographic_parity(adult_test, clf, "Race")
+# adult.evaluate_demographic_parity(adult_test, clf, "Sex")
+# adult.evaluate_demographic_parity(adult_test, clf, "Country")
+# adult.evaluate_demographic_parity(adult_test, clf, "Age")
+#
+# adult.evaluate_equality_of_opportunity(adult_test, clf, "Race")
+# adult.evaluate_equality_of_opportunity(adult_test, clf, "Sex")
+# adult.evaluate_equality_of_opportunity(adult_test, clf, "Country")
+# adult.evaluate_equality_of_opportunity(adult_test, clf, "Age")
